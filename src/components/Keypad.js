020862739ded0ee7ee4828c3c92fbdcf3e820a9b
Joyce Lee <joycelee901@gmail.com>
2021-06-09 14:21:59 -0500

    commit tree e14c3eff1e0d0920e7a2f6200d681905153b69b4

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+function Keypad() {
+    function password() {
+        console.log("Entering password...")
+    }
+
+    return (
+        <input type="password" onChange={password}/>
+    )
+}
+
+export default Keypad;
